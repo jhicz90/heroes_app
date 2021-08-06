d96f3e4590a69c0909126b344605471efdac5203
jhicz90 <jhicz90@gmail.com>
2021-08-06 14:49:51 -0500

    aprendiendo REDUCER y USECONTEXT

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+import { AuthContext } from './auth/AuthContext'
+import { AuthReducer } from './auth/AuthReducer'
 import { AppRouter } from './routes/AppRouter'
 
+const init = () => {
+    return JSON.parse(localStorage.getItem('hero-app-user')) || { logged: false }
+}
+
 export const HeroesApp = () => {
+
+    const [user, dispatch] = useReducer(AuthReducer, {}, init)
+
     return (
-        <AppRouter />
+        <AuthContext.Provider value={{ user, dispatch }}>
+            <AppRouter />
+        </AuthContext.Provider>
     )
 }
